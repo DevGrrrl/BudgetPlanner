fe6e397589b05b3dab5af44b09a56fc914908cb4
Lex <alexisjaynedefoe@gmail.com>
2017-11-30 11:06:06 +0000

    :construction:create inputHandler & send object to queries -  relates to issue #1

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const querystring = require('querystring');
 
 const homeHandler = (request, response) => {
     fs.readFile(path.join(__dirname, '..', 'public', 'index.html'), 'utf8', (err, file) => {
@@ -25,8 +26,8 @@ const staticFileHandler = (request, response, endpoint) => {
             js: 'application/javascript',
         }
         /* UPADATE THIS*/
-    const extension = url.split('.')[1];
-    const filePath = path.join(__dirname, '..', url);
+    const extension = endpoint.split('.')[1];
+    const filePath = path.join(__dirname, '..', endpoint);
     fs.readFile(filePath, (err, file) => {
         if (err) response.end('error');
         response.writeHead(200, 'Content-Type: ' + extensionType[extension]);
@@ -34,4 +35,16 @@ const staticFileHandler = (request, response, endpoint) => {
     })
 };
 
-module.exports { homeHandler, staticFileHandler }
+const inputHandler = (request, response, endpoint) => {
+
+    var allTheData = '';
+    request.on('data', function(chunckOfData){
+      allTheData += chunckOfData;
+    });
+    request.on('end', function(){
+      const parsed = querystring.parse(allTheData);
+      checkUser(parsed);
+    })
+
+};
+module.exports = { homeHandler, staticFileHandler, inputHandler }
