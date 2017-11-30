00ccac3998593078cc8190a1f2ebe933fd67ad93
hannahcmtucker <hannahcmtucker@gmail.com>
2017-11-30 12:34:27 +0000

    Add database queries

--- tests/tests.js
+++ tests/tests.js
@@ -3,8 +3,12 @@ const shot = require('shot');
 // const router = require('./src/router');
 // const { homeHandler, staticFileHandler, resultsHandler } = require('./src/handler');
 const runDbBuild = require("../src/database/db_build");
-const getCostPerPerson = require('../src/queries/get_data.js')
-const testCheckUser = require('../src/queries/check_user.js')
+const getCostPerPerson = require('../src/queries/get_costs_per_person.js')
+const checkUser = require('../src/queries/check_user.js')
+const createUser = require('../src/queries/create_user.js')
+const setNewItem = require('../src/queries/set_new_item.js')
+const getUser = require('../src/queries/test_queries/test_get_user.js')
+const getItem = require('../src/queries/test_queries/test_get_item.js')
 
 
 test('Tape is working', (t) => {
@@ -15,29 +19,61 @@ test('Tape is working', (t) => {
 
 test('getCostPerPerson', (t) => {
     runDbBuild(function(err, res) {
-        let expected = [{ user_name: 'Alina', sum: 9.3 }, { user_name: 'James', sum: 8.26 } ];
+        let expected = [{ user_name: 'Alina', sum: 9.3 }, { user_name: 'James', sum: 8.26 }];
         getCostPerPerson((err, res) => {
             if (err) console.log(err)
+            console.log(res)
             t.deepEqual(expected, res, 'Should return summed cost per person')
             t.end();
         })
     })
 });
 
-test('test check_user', (t) => {
+test('test checkUser', (t) => {
     runDbBuild(function(err, res) {
-        let newItem = {userName:'Alina'}
-        testCheckUser(newItem,(err, res) => {
+        let newItem = { userName: 'Alina' }
+        checkUser(newItem, (err, res) => {
             if (err) console.log(err)
-          t.equal(1, res, 'Should return 1 if name is present in db');
+            t.equal(1, res, 'Should return 1 if name is present in db');
         })
     })
     runDbBuild(function(err, res) {
-        let newItem = {userName:'Hannah'}
-        testCheckUser(newItem,(err, res) => {
+        let newItem = { userName: 'Hannah' }
+        checkUser(newItem, (err, res) => {
             if (err) console.log(err)
-          t.equal(0, res, 'Should return 0 if name is not present in db')
+            t.equal(0, res, 'Should return 0 if name is not present in db')
             t.end();
+        })
+    })
+});
+
+test('test createUser', (t) => {
+    runDbBuild(function(err, res) {
+        let newItem = { userName: 'Hannah' }
+        createUser(newItem, (err, res) => {
+            if (err) console.log(err)
+            getUser((err, res) => {
+                t.equal(3, res.length, "Should be new row in users table (total 3)")
+                t.end();
+            })
+        })
+    })
+});
+
+test('test setNewItem', (t) => {
+    runDbBuild(function(err, res) {
+        let newItem = {
+            userName: 'Alina',
+            cost: 2.45,
+            category: 'Groceries',
+            datePurchased: new Date('2017-11-29'),
+        }
+        setNewItem(newItem, (err, res) => {
+            if (err) console.log(err)
+            getItem((err, res) => {
+                t.equal(4, res.length, "Should be new row in items table (total 4)")
+                t.end();
+            })
         })
     })
 });
