f2dbec0299a4967d2da907070be21edb03db8b2d
parkhabit <parkhabit@gmail.com>
2017-11-30 10:03:35 +0000

    changed xhr request to use form

--- public/index.js
+++ public/index.js
@@ -9,17 +9,17 @@ var btn = document.getElementById('generateCost');
 
 
 //event listener on form input, then call request
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  var itemObject = {
-    userName : user.value,
-    categoryType : category.value,
-    cost : itemCost.value,
-    date :datePurchased.value
-  }
-  var url = '/input';
-  request(url, 'POST', cb, itemObject);
-});
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   var itemObject = {
+//     userName : user.value,
+//     categoryType : category.value,
+//     cost : itemCost.value,
+//     date :datePurchased.value
+//   }
+//   var url = '/input';
+//   request(url, 'POST', cb, itemObject);
+// });
 
 //generic http request, what do we want the callback function to do? render?
 function request(url, method, cb, body) {
