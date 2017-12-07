c8d3e90790520236d35e034ae70289673c3782d4
parkhabit <parkhabit@gmail.com>
2017-12-07 14:29:48 +0000

    seperated function from login.hmtl

--- public/login.js
+++ public/login.js
@@ -4,19 +4,22 @@ var signupPassword1 = document.getElementById('signupPW1');
 var signupPassword2 = document.getElementById('signupPW2');
 var validateSignup = document.getElementById('validateMsgSignup');
 var validateLogin = document.getElementById('validateMsgLogin');
+var formSubmit = document.getElementById('form-signup');
 
 
-function checkPassword(){
-  if (validateSignup.childElementCount > 0) {
-    validateSignup.removeChild(validateSignup.lastChild);
+formSubmit.addEventListener('submit', function(event){
+  function checkPassword(){
+    if (validateSignup.childElementCount > 0) {
+      validateSignup.removeChild(validateSignup.lastChild);
+    }
+    var form_valid = (signupPassword1.value === signupPassword2.value );
+    if(!form_valid){
+    var pMsg = document.createElement('p');
+    var errorMsg = document.createTextNode('Passwords do not match');
+    pMsg.appendChild(errorMsg);
+    validateSignup.appendChild(pMsg);
+  return false;
   }
-  var form_valid = (signupPassword1.value === signupPassword2.value );
-  if(!form_valid){
-  var pMsg = document.createElement('p');
-  var errorMsg = document.createTextNode('Passwords do not match');
-  pMsg.appendChild(errorMsg);
-  validateSignup.appendChild(pMsg);
-return false;
-}
-return true;
-}
+  return true;
+  }
+})
