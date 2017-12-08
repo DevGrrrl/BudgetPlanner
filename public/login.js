2e96f1d3efeff9e58bf5e9c26507afe0a1f73857
parkhabit <parkhabit@gmail.com>
2017-12-08 11:34:12 +0000

    fixed password validation on front end relates to #74

--- public/login.js
+++ public/login.js
@@ -7,35 +7,14 @@ var validateLogin = document.getElementById('validateMsgLogin');
 var formSubmit = document.getElementById('form-signup');
 
 
-formSubmit.addEventListener('submit', function(event) {
+signupPassword2.addEventListener('input', function(event) {
     if (validateSignup.childElementCount > 0) {
         validateSignup.removeChild(validateSignup.lastChild);
     }
-    var form_valid = (signupPassword1.value === signupPassword2.value);
-    if (!form_valid) {
+    if (signupPassword1.value !== signupPassword2.value) {
         var pMsg = document.createElement('p');
         var errorMsg = document.createTextNode('Passwords do not match');
         pMsg.appendChild(errorMsg);
         validateSignup.appendChild(pMsg);
-        return false;
     }
-    return true;
-})
-
-
-//generic xhr XMLHttpRequest
-function request(url, method, cb, body) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                cb(null, JSON.parse(xhr.responseText));
-            } else {
-                var errorMessage = JSON.parse(xhr.responseText);
-                cb("Error" + url + " " + errorMessage);
-            }
-        }
-    };
-    xhr.open(method, url, true);
-    xhr.send(body);
-}
+});
